110b0d26047d4eb4757924b6a440f43e9be19f0f
cryeer <shoufenzheng@qq.com>
2020-04-10 17:40:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const fs = require('fs')
 
 
 const url = 'https://oapi.dingtalk.com/robot/send?access_token=e1218776ef269757b57cce0c3a0403d381df1f625a2c69b7848b4d1caf4d78fa'
@@ -10,28 +11,4 @@ const data = {
 }
 
 axios.post(url, data)
-
-// var http = require('http');
-// var querystring =require('querystring');
-// var post_data = querystring.stringify(data);
-// var options = {
-//                    hostname:'oapi.dingtalk.com',     //此处不能写协议，如 ： http://,https://  否则会报错
-//                    port:80,
-//                    path:'/robot/send?access_token=e1218776ef269757b57cce0c3a0403d381df1f625a2c69b7848b4d1caf4d78fa',
-//                    method:'POST',
-//                    headers: {
-//                         'Content-Type':'application/x-www-form-urlencoded',
-//                         'Content-Length': post_data.length
-//                    }
-//                };
-// var req = http.request(options,function(res){
-//                         console.log('STATUS:'+res.statusCode);
-//                         console.log('HEADERS:'+JSON.stringify(res.headers));
-//                         res.setEncoding('utf8');
-//                         res.on('data',function(chunk){
-//                                 console.log('BODY:'+chunk);
-//                         });
-//            });
-// // write data to request body
-// req.write(post_data);
-// req.end();
+fs.writeFileSync(`./dist/${Date.now()}`, Date.now())
