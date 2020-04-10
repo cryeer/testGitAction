1e3e0864d82d6a93acf8cba440f2b1f848dc1d5a
cryeer <shoufenzheng@qq.com>
2020-04-10 15:14:23 +0800

    send to dingtalk

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const fs = require('fs')
-const path = require('path')
+const axios = require('axios')
 
-fs.writeFileSync(path.join(__dirname, './package'), 'i am a package')
+
+const url = 'https://oapi.dingtalk.com/robot/send?access_token=e1218776ef269757b57cce0c3a0403d381df1f625a2c69b7848b4d1caf4d78fa'
+const data = {
+    "msgtype": "text", 
+    "text": {
+        "content": "我就是git, 是不一样的G~"
+    }
+}
+
+axios.post(url, data)
