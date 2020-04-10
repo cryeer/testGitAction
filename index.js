bc3d7b9dee92c33b9de8b149a414c86ee69b113d
cryeer <shoufenzheng@qq.com>
2020-04-10 16:35:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const axios = require('axios')
+// const axios = require('axios')
 
 
-const url = 'https://oapi.dingtalk.com/robot/send?access_token=e1218776ef269757b57cce0c3a0403d381df1f625a2c69b7848b4d1caf4d78fa'
+// const url = 'https://oapi.dingtalk.com/robot/send?access_token=e1218776ef269757b57cce0c3a0403d381df1f625a2c69b7848b4d1caf4d78fa'
 const data = {
     "msgtype": "text", 
     "text": {
@@ -9,4 +9,29 @@ const data = {
     }
 }
 
-axios.post(url, data)
+// axios.post(url, data)
+
+var http = require('http');
+var querystring =require('querystring');
+var post_data = querystring.stringify(data);
+var options = {
+                   hostname:'oapi.dingtalk.com',     //此处不能写协议，如 ： http://,https://  否则会报错
+                   port:80,
+                   path:'/robot/send?access_token=e1218776ef269757b57cce0c3a0403d381df1f625a2c69b7848b4d1caf4d78fa',
+                   method:'POST',
+                   headers: {
+                        'Content-Type':'application/x-www-form-urlencoded',
+                        'Content-Length': post_data.length
+                   }
+               };
+var req = http.request(options,function(res){
+                        console.log('STATUS:'+res.statusCode);
+                        console.log('HEADERS:'+JSON.stringify(res.headers));
+                        res.setEncoding('utf8');
+                        res.on('data',function(chunk){
+                                console.log('BODY:'+chunk);
+                        });
+           });
+// write data to request body
+req.write(post_data);
+req.end();
